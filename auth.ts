0010bbd10194c535f9bb337d6554a3e857fdbcc6
Neil Mahajan <neilsmahajan@gmail.com>
2025-07-04 12:09:10 -0700

    feat: add authentication handlers and dependencies for NextAuth

---
+++ auth.ts
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth";
+import { UpstashRedisAdapter } from "@auth/upstash-redis-adapter";
+import { Redis } from "@upstash/redis";
+import Google from "next-auth/providers/google";
+
+const redis = new Redis({
+  url: process.env.KV_REST_API_URL!,
+  token: process.env.KV_REST_API_TOKEN!,
+});
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: UpstashRedisAdapter(redis),
+  providers: [Google],
+});
